dc79f1df77781cba0f838f45c61b79e130fa75ca
Billy <you199037@163.com>
2019-06-05 16:05:23 +0800

    增加flyway对脚本进行管理

---
+++ family-server/src/main/resources/db/migration/V1.0.0.201906051457__family_db.sql
@@ -0,0 +1,18 @@
+
+create table family_members(
+	id varchar(32) not null,
+	name varchar(200) not null,
+	age int2,
+	birthday date,
+	family_status varchar(20) not null,
+	personal_profile text,
+	create_time timestamp,
+	creator varchar(200),
+	update_time timestamp,
+	updater varchar(200),
+	comments varchar(400),
+	constraint pk_family_members_id primary key (id)
+);
+
+comment on table family_members is '家庭成员';
+comment on column family_members.id is '主键';
