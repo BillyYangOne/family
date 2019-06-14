bdc5308d14c3212b73f98326e571d1e3caafd6f9
Billy <you199037@163.com>
2019-06-14 18:33:32 +0800

    集成swagger

--- family-server/src/main/java/com/billy/system/controller/MemberManageController.java
+++ family-server/src/main/java/com/billy/system/controller/MemberManageController.java
@@ -14,7 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Api("MemberManageController")
+@Api(tags = "MemberManageController")
 @RestController
 @RequestMapping("member")
 public class MemberManageController extends BaseController {
